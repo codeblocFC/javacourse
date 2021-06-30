0b027ffed8fe4d8263844672bc6a95a83c3d6e1a
codeblocFC <angelnegrogenesis@hotmail.com>
2021-06-29 21:02:16 -0500

    Secund commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,44 @@
 public class App {
+
+    private String nombre;
+    private int edad;
+    private char sexo;
+    private float peso;
+    private boolean estadoCivil;
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        //Tipos datos y operadores
+        String res="Hola";
+        int resultado=15+32;
+        double resultado2=25/3;
+        float resultado3=58*21;
+        int resultado4=21%3;
+        //operadores unarios
+        resultado++;
+        resultado--;
+        resultado =-resultado;
+        resultado=+resultado;
+        //operaadores logicos
+        boolean respuesta=true;
+        respuesta=5>8;
+        respuesta=5<8;
+        respuesta=5>=8;
+        respuesta=5!=8;
+        respuesta=5==8;
+        respuesta=12<5 && 4>1;
+        respuesta=14<25 || 1>5;
+        //operadores bit a bit
+        //&
+        //^
+        //|
+        //Estructuras de control
+        if (respuesta) {
+            
+        }
+        if (respuesta) {
+            
+        } else {
+            
+        }
     }
 }
