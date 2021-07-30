3e8c1ad7dec4befe68e8a361bb3a29e9a9a6190e
codeblocFC <angelnegrogenesis@hotmail.com>
2021-07-29 19:19:21 -0500

    Working with MVC first Controller

--- last/src/views/Interfaz.java
+++ last/src/views/Interfaz.java
@@ -1,11 +1,16 @@
 package views;
 
 import javax.swing.JFrame;
+
+import controller.Controlador;
+
 import java.awt.BorderLayout;
+
 
 public class Interfaz extends JFrame{
 
     private PanelOpciones panelOpciones;
+    private Controlador controlador;
 
     public Interfaz() {
         super();
@@ -14,9 +19,20 @@ public class Interfaz extends JFrame{
 
         setTitle("Wallet");
         setLayout(new BorderLayout());
+
+        controlador = new Controlador();
+
         //panelOpciones=new PanelOpciones();
         panelOpciones=new PanelOpciones(this); //muestraventana en medio del frame interfaz
         add(panelOpciones,BorderLayout.SOUTH);
+    }
+
+    public void addClient(String nombre){
+        controlador.addClient(nombre);
+    }
+
+    public String listarClientes(){
+        return controlador.listarClientes();
     }
 
     public static void main(String[] args) {
--- last/src/views/PanelOpciones.java
+++ last/src/views/PanelOpciones.java
@@ -13,16 +13,20 @@ public class PanelOpciones extends JPanel implements ActionListener{
 
     public final static String BTN_SALDO="Consultar Saldo";
     public final static String BTN_CARGAR="Cargar";
+    public final static String BTN_CREAR="Crear Clientes";
+    public final static String BTN_LISTAR="Listar Clientes";
 
     //crear relaciones o atributos
     private JButton btnSaldo;
     private JButton btnCargar;
+    private JButton btnCrear;
+    private JButton btnListar;
 
     private Interfaz interfaz;
 
     public PanelOpciones(Interfaz i) {
         super();
-        setLayout(new GridLayout(2,1));
+        setLayout(new GridLayout(1,4));
         setBorder(new TitledBorder("Opciones"));
 
         interfaz =i;
@@ -30,18 +34,41 @@ public class PanelOpciones extends JPanel implements ActionListener{
         //inicializar atributos
         btnSaldo=new JButton(BTN_SALDO);
         btnCargar=new JButton(BTN_CARGAR);
+        btnCrear=new JButton(BTN_CREAR);
+        btnListar=new JButton(BTN_LISTAR);
 
         //modificar las propiedades
         btnSaldo.setActionCommand(BTN_SALDO);
         btnCargar.setActionCommand(BTN_CARGAR);
+        btnCrear.setActionCommand(BTN_CREAR);
+        btnListar.setActionCommand(BTN_LISTAR);
 
         btnSaldo.addActionListener(this);
         btnCargar.addActionListener(this);
+        btnCrear.addActionListener(this);
+        btnListar.addActionListener(this);
 
         //modificar las relaciones o atributos
         add(btnSaldo);
         add(btnCargar);
+        add(btnCrear);
+        add(btnListar);
 
+    }
+
+    public void addClient(){
+        String nombre=JOptionPane.showInputDialog(interfaz, "Digita el nombre del cliente");
+        if(nombre!=null && !nombre.equals("")){
+            interfaz.addClient(nombre);
+        }
+        else if(nombre!=null){
+            JOptionPane.showMessageDialog(interfaz, "Revisa los datos");
+        }
+        
+    }
+
+    public void listarClientes(){
+        JOptionPane.showMessageDialog(interfaz, interfaz.listarClientes());
     }
 
     @Override //metodo sobrecargado
@@ -54,8 +81,14 @@ public class PanelOpciones extends JPanel implements ActionListener{
             }
         else if(e.getActionCommand().equals(btnCargar.getActionCommand())){
             System.out.println("Estas cargando");
-            JOptionPane.showMessageDialog(interfaz,"Vas a recarga", "Recarga Wallet", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(interfaz,"Vas a recargar", "Recarga Wallet", JOptionPane.WARNING_MESSAGE);
         }
+        else if(e.getActionCommand().equals((BTN_CREAR))){
+            addClient();
+            }
+        else if(e.getActionCommand().equals((BTN_LISTAR))){
+            listarClientes();
+            }
     }
     
 }
